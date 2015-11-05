1cb5c4748399908a31627b724e86dd3d3c273570
Ben Spitz <benespitz>
2015-11-04 21:29:11 -0800

    Added Totient calculation. Progress is very far away. We request more coffee.

--- affine.cpp
+++ affine.cpp
@@ -4,11 +4,25 @@ using namespace std;
 
 struct node {
    int val;
-
    node* next;
+};
+
+int gcd (int x, int y) {
+  if (y == 0)
+    return x;
+  return gcd(y, x % y);
+}
+
+int totient (int x) {
+  int result = 0;
+  for (int i = 1; i < x; i++) {
+    if (gcd(i,x) == 1)
+      result++;
+  }
+  return result;
 }
 
 int main() {
-
+   cout << "Totient of 26 is " << totient(26) << endl;
    return 0;
 }
